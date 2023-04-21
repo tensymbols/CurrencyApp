7bdb0771d664dff21b0681259ec2e058ea998fc5
tensymbols <thetensymbols@gmail.com>
2023-04-21 21:55:45 +0300

    date

--- internal/date/date.go
+++ internal/date/date.go
@@ -10,13 +10,12 @@ func DateDecrease(t *time.Time) {
 }
 
 func GetStringDate(t time.Time) string {
-	y, m, d := t.Date()                                            // год месяц день
-	sy, sm, sd := fmt.Sprint(y), fmt.Sprint(int(m)), fmt.Sprint(d) // год месяц день в виде строки
-	// добавляем к месяцу 0 если он меньше 10 для корректного формата
+	y, m, d := t.Date()
+	sy, sm, sd := fmt.Sprint(y), fmt.Sprint(int(m)), fmt.Sprint(d)
+
 	if m < 10 {
 		sm = "0" + sm
 	}
-	// добавляем к дню 0 если он меньше 10 для корректного формата
 	if d < 10 {
 		sd = "0" + sd
 	}
